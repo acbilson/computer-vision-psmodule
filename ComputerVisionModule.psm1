f14e419312445e0b53345f464fecf1fcc08d70be
Alex Bilson <alex.bilson@spr.com>
2019-08-02 12:25:17 -0500

    moves script into a module for easier distribution

--- ComputerVisionModule.psm1
+++ ComputerVisionModule.psm1
@@ -1,13 +1,37 @@
-########################################
-# Constants
-########################################
-$BASE_URI = 'https://westcentralus.api.cognitive.microsoft.com/'
+<#
+    Computer Vision API Script Module
+    Author: Alex Bilson (alex.bilson@spr.com)
+    Last Updated: 2-August-2019
+
+    Description:
+    
+        This script module simplifies the use of the Computer Vision API to analyze images
+
+    Setup:
+
+        This script requires an Azure Cognitive Service account. Please supply the example values in a settings.json file in the same path as this script.
+
+    Example:
+
+        {
+            'host': 'westcentralus.api.cognitive.microsoft.com', # no forward slashes please
+            key: 'sfslkja;sldj;' # the subscription key given you by Microsoft when you register your account. there are two keys; the first is fine
+        }
+
+#>
 
 ########################################
-# Helper Functions
+# Helper Functions (not exported by module)
 ########################################
 
-# Adds function to simplify the http request
+# Reads the settings from .\settings.json
+function Read-Settings {
+
+    $settings = Get-Content -Path .\settings.json -Raw | ConvertFrom-Json
+    return $settings
+}
+
+# Simplifies the http request
 function New-HttpRequestUri
 {
     [CmdletBinding()]
@@ -15,13 +39,13 @@ function New-HttpRequestUri
     (
         [Parameter(Mandatory = $true)]
         [String]
-        $BaseUri,
+        $HostName,
 
         [Parameter(Mandatory = $true)]
         [String]
         $Path,
  
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false)]
         [Hashtable]
         $QueryParameters
     )
@@ -38,33 +62,59 @@ function New-HttpRequestUri
     }
  
     # Build the uri
-    $uriRequest = [System.UriBuilder]$BaseUri
-    $uriRequest.Query = $nvCollection.ToString()
-    $uriRequest.Path = $Path
+    $builder = New-Object System.UriBuilder -Property @{
+    
+        'host'=$HostName;
+        'scheme'='https';
+        'port'=443;
+        'query'=$nvCollection.ToString();
+        'path'=$Path
+    }
  
-    return $uriRequest.Uri.OriginalString
+    return $builder.Uri.OriginalString
 }
 
-# Analyzes the content of an image, returning information about category, tags, description, brands and more (edit to change information)
+########################################
+# Computer Vision API Functions
+########################################
+
+# I did attempt to make this a pipeable function, but ran into endless loops I couldn't figure out
 function Request-AnalyzeImage {
+<#
+        .SYNOPSIS
+            
+            Analyzes the content of an image, returning information about category, tags, description, and brands.
+
+        .OUTPUTS
+
+            Returns a PSCustomObject with the following properties:
+            
+        .EXAMPLE
+
+            $imageBytes = [System.IO.File]::ReadAllBytes('C:\User\myusername\Images\test-image.png'); Request-AnalyzeImage -ImageBytes $imageBytes
+
+    #>
     [CmdletBinding()]
     param 
     (
+        # A byte array of the image. See example for how to retrieve image bytes.
         [Parameter(Mandatory = $true)]
-        [Byte[]]
-        $ImageBytes
+        [Byte[]]$ImageBytes
     )
+
+    # Retrieve settings from file
+    $settings = Read-Settings
 
     # Modify these to retrieve different information from the analysis
     $queryParams = @{
         'visualFeatures'='Brands,Categories,Description,Tags'
     }
 
-    $uri = New-HttpRequestUri -BaseUri $BASE_URI -Path '/vision/v2.0/analyze' -QueryParameter $queryParams
+    $uri = New-HttpRequestUri -HostName $settings.host -Path '/vision/v2.0/analyze' -QueryParameter $queryParams
 
     $response = curl `
     -Uri $uri `
-    -Headers @{ "Content-Type"="application/octet-stream"; "Ocp-Apim-Subscription-Key"="66f52ef9ac114042bee47b446019147a" } `
+    -Headers @{ "Content-Type"="application/octet-stream"; "Ocp-Apim-Subscription-Key"=$settings.key } `
     -Method POST `
     -UseBasicParsing `
     -Body $ImageBytes
@@ -72,14 +122,27 @@ function Request-AnalyzeImage {
     return $response
 }
 
-# Reads text in an image, returning both the OCR'd text and it's location in the image
 function Request-RecognizeText {
+    <#
+        .SYNOPSIS
+            
+            Reads text in an image, returning both the OCR'd text and it's location in the image
+
+        .OUTPUTS
+
+            Returns a PSCustomObject with the following properties:
+            
+        .EXAMPLE
+
+            $imageBytes = [System.IO.File]::ReadAllBytes('C:\User\myusername\Images\test-image.png'); Request-RecognizeText -ImageBytes $imageBytes
+
+    #>
     [CmdletBinding()]
     param 
     (
+        # A byte array of the image. See example for how to retrieve image bytes.
         [Parameter(Mandatory = $true)]
-        [Byte[]]
-        $ImageBytes
+        [Byte[]]$ImageBytes
     )
 
     # Modify these to retrieve different information from the analysis
@@ -145,11 +208,25 @@ function Request-RecognizeText {
     return $response
 }
 
-# Returns only the tags, a subset of the analyize image functionality
 function Request-TagImage {
+    <#
+        .SYNOPSIS
+            
+            Returns image tags, a subset of the analyize image functionality
+
+        .OUTPUTS
+
+            Returns a PSCustomObject with the following properties:
+            
+        .EXAMPLE
+
+            $imageBytes = [System.IO.File]::ReadAllBytes('C:\User\myusername\Images\test-image.png'); Request-RecognizeText -ImageBytes $imageBytes
+
+    #>
     [CmdletBinding()]
     param 
     (
+        # A byte array of the image. See example for how to retrieve image bytes.
         [Parameter(Mandatory = $true)]
         [Byte[]]
         $ImageBytes
@@ -172,19 +249,6 @@ function Request-TagImage {
     return $response
 }
 
-########################################
-# Read image bytes and send request
-########################################
-
-$imagePath = [System.IO.Path]::Combine($env:OneDriveCommercial, 'Archive\2019\GroceryProject\Images\test-image-cereal.png')
-$imageBytes = [System.IO.File]::ReadAllBytes($imagePath)
-
-$response = Request-AnalyzeImage -ImageBytes $imageBytes
-
-$result = $null
-if ($response.StatusCode -eq 200)
-{
-    $result = ConvertFrom-Json -InputObject $response.Content
-}
-
-$result
+Export-ModuleMember -function Request-AnalyzeImage
+Export-ModuleMember -function Request-RecognizeText
+Export-ModuleMember -function Request-TagImage
